700605cd50506fd9aeb8598755c2e5751053bcf1
Joshua Pease <joshua.pease@viget.com>
2024-05-21 14:19:47 -0700

    Styles, transitions, toggle when clicking same level or outside

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -17,20 +17,19 @@ Alpine.data("nav", () => ({
   toggle(el) {
     const index = parseInt(el.dataset.index, 10);
     const currentIndex = this.stack.length - 1;
-    if (index < currentIndex) {
+    const shouldPush = !this.isInStack(el);
+    if (index <= currentIndex) {
       // Remove everything down to the index you clicked.
       this.popStack(currentIndex - index + 1);
     }
     // If you clicked a toggle outside of the current stack, push it to the stack
-    if (!this.isInStack(el)) {
+    if (shouldPush) {
       this.stack.push(el.id);
     }
   },
   shouldTrap(el) {
+    // You could modify this to only trap at a mobile sized match media
     return this.isInStack(el);
-  },
-  isHidden(el) {
-    return !this.isInStack(el);
   },
   isInStack(el) {
     return this.stack.includes(el.id);
