212614535b6fca4b56221d58cb62023ac08c0fce
Joshua Pease <joshua.pease@viget.com>
2024-05-21 13:54:42 -0700

    Clean up & simplify code

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,43 +8,30 @@ Alpine.plugin(focus);
 Alpine.data("nav", () => ({
   /** @type {string[]} */
   stack: [],
-
-  popStack() {
-    this.stack = this.stack.slice(0, -1);
+  popStack(popTimes = 1) {
+    this.stack = this.stack.slice(0, popTimes * -1);
   },
-
+  resetStack() {
+    this.stack = [];
+  },
   toggle(el) {
-    const indexStr = el.dataset.index;
-    const id = el.id;
-    const index = parseInt(indexStr, 10);
+    const index = parseInt(el.dataset.index, 10);
     const currentIndex = this.stack.length - 1;
-
-    if (index > currentIndex) {
-      this.stack.push(id);
-    } else {
-      const popTimes = currentIndex - index + 1;
-      const idInCurrentStack = this.stack.includes(id);
-
-      // Remove everything down to the level you clicked.
-      for (let i = 0; i < popTimes; i++) {
-        this.popStack();
-      }
-
-      // If you clicked a toggle outside of the current stack, push it to the stack
-      if (!idInCurrentStack) {
-        this.stack.push(id);
-      }
+    if (index < currentIndex) {
+      // Remove everything down to the index you clicked.
+      this.popStack(currentIndex - index + 1);
+    }
+    // If you clicked a toggle outside of the current stack, push it to the stack
+    if (!this.isInStack(el)) {
+      this.stack.push(el.id);
     }
   },
-
   shouldTrap(el) {
     return this.isInStack(el);
   },
-
   isHidden(el) {
     return !this.isInStack(el);
   },
-
   isInStack(el) {
     return this.stack.includes(el.id);
   },
