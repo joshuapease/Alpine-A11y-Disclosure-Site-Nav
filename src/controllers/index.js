3b24d7e0e016d8433b763167f1e79484acffb56f
Joshua Pease <joshua.pease@viget.com>
2024-05-21 10:48:15 -0700

    Round 1

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,53 @@
-import { Application } from "@hotwired/stimulus"
-import StimulusControllerResolver, { createViteGlobResolver } from 'stimulus-controller-resolver'
+import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
 
-const application = Application.start()
+window.Alpine = Alpine;
 
-StimulusControllerResolver.install(application, createViteGlobResolver(
-  import.meta.glob('./**/*_controller.js')
-))
+Alpine.plugin(focus);
+
+Alpine.data("nav", () => ({
+  /** @type {string[]} */
+  stack: [],
+
+  popStack() {
+    this.stack = this.stack.slice(0, -1);
+  },
+
+  toggle(el) {
+    const indexStr = el.dataset.index;
+    const id = el.id;
+    const index = parseInt(indexStr, 10);
+    const currentIndex = this.stack.length - 1;
+
+    if (index > currentIndex) {
+      this.stack.push(id);
+    } else {
+      const popTimes = currentIndex - index + 1;
+      const idInCurrentStack = this.stack.includes(id);
+
+      // Remove everything down to the level you clicked.
+      for (let i = 0; i < popTimes; i++) {
+        this.popStack();
+      }
+
+      // If you clicked a toggle outside of the current stack, push it to the stack
+      if (!idInCurrentStack) {
+        this.stack.push(id);
+      }
+    }
+  },
+
+  shouldTrap(el) {
+    return this.isInStack(el);
+  },
+
+  isHidden(el) {
+    return !this.isInStack(el);
+  },
+
+  isInStack(el) {
+    return this.stack.includes(el.id);
+  },
+}));
+
+Alpine.start();
